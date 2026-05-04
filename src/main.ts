fb0f130f7b0afe9d2bf30e29da528cf6399bda13
Jiří Motejzík <motejzik@praguebest.cz>
2026-05-04 09:04:01 +0200

    add basic design system

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
 import './assets/main.css'
+import './styles/global.scss'
 
 const app = createApp(App)
 
