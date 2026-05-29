6e65a58139fbef761bc26d4b81d9b56abb69d41d
Jiří Motejzík <motejzik@praguebest.cz>
2026-05-29 12:23:35 +0200

    add useDayDetail service

---
+++ src/modules/weather/services/useDayDetail.ts
@@ -0,0 +1,28 @@
+import { useOpenMeteoApi } from '~/api/openMeteo/useOpenMeteoApi'
+import { useGeolocationStore } from '~/modules/geolocation/stores/useGeolocationStore'
+import { toMinutelySummary } from '~/modules/weather/mappers/toMinutelySummary'
+import { toTemperatureChart } from '~/modules/weather/mappers/toTemperatureChart'
+import { toPrecipitationChart } from '~/modules/weather/mappers/toPrecipitationChart'
+import type { MinutelyResponse } from '~/modules/weather/types/MinutelyResponse'
+
+export const useDayDetail = () => {
+    const api = useOpenMeteoApi()
+    const geolocationStore = useGeolocationStore()
+
+    return async (date: string) => {
+        const { latitude, longitude } = geolocationStore.location
+        const response = await api.get<MinutelyResponse>('/forecast', {
+            latitude,
+            longitude,
+            minutely_15: 'temperature_2m,apparent_temperature,precipitation,sunshine_duration,weather_code,is_day',
+            start_minutely_15: `${date}T00:00`,
+            end_minutely_15: `${date}T23:45`,
+        })
+
+        return {
+            summary: toMinutelySummary(response),
+            temperatureChart: toTemperatureChart(response),
+            precipitationChart: toPrecipitationChart(response),
+        }
+    }
+}
