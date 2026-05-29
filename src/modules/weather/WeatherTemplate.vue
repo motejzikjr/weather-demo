3a3cf711bf8f9783402db8f5e30cf41ef83ad576
Jiří Motejzík <motejzik@praguebest.cz>
2026-05-29 12:11:48 +0200

    add DayDetail page

--- src/modules/weather/WeatherTemplate.vue
+++ src/modules/weather/WeatherTemplate.vue
@@ -12,6 +12,14 @@
         v-if="data"
         :variant="FloorVariant.PRIMARY"
     >
+        <div class="grid grid-cols-2 gap-4 sm:grid-cols-4 lg:grid-cols-7">
+            <DailyWeatherCard
+                v-for="day in data.forecast"
+                :key="day.date"
+                :day="day"
+                :on-detail="() => router.push('/weather/day/' + day.date)"
+            />
+        </div>
         <ForecastChart :forecast="data.forecast" />
     </Floor>
 </template>
@@ -19,6 +27,7 @@
 <script setup lang="ts">
     import { ref, watch } from 'vue'
     import { storeToRefs } from 'pinia'
+    import { useRouter } from 'vue-router'
     import { useGetWeather } from '~/modules/weather/services/useGetWeather'
     import { useWeatherStore } from '~/stores/useWeatherStore'
     import { useGeolocationStore } from '~/modules/geolocation/stores/useGeolocationStore'
@@ -26,6 +35,7 @@
     import Floor from '~/ui/Floor/Floor.vue'
     import CurrentWeatherCard from '~/ui/CurrentWeatherCard/CurrentWeatherCard.vue'
     import ForecastChart from '~/ui/ForecastChart/ForecastChart.vue'
+    import DailyWeatherCard from '~/ui/DailyWeatherCard/DailyWeatherCard.vue'
     import { FloorVariant } from '~/ui/Floor/FloorVariant'
     import { CurrentWeather } from '~/modules/weather/types/CurrentWeather'
     import { DailyForecast } from '~/modules/weather/types/DailyForecast'
@@ -35,6 +45,7 @@
         forecast: DailyForecast[]
     }
 
+    const router = useRouter()
     const geolocationStore = useGeolocationStore()
     const { location } = storeToRefs(geolocationStore)
     const getWeather = useGetWeather()
---
+++ src/views/DayDetailView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Detail dne: {{ date }}</div>
+</template>
+
+<script setup lang="ts">
+    import { useRoute } from 'vue-router'
+
+    const route = useRoute()
+    const date = route.params.date
+</script>
